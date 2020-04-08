bea486a8c13aff31a1701bdd7874f89312897fca
Mikhail <ca8paco8@gmail.com>
2020-04-08 23:33:34 +0300

    Fixed bug in evaluation function for AlphaBetaBot

--- include/AlphaBetaBot.hpp
+++ include/AlphaBetaBot.hpp
@@ -37,18 +37,31 @@ namespace evaluate
 	static int scoreSumFigurePoints(const Board& board, const PlayerColour colour)
 	{
 		int sum = 0;
+		bool enemy_intellector = 0;
+		bool allied_intellector = 0;
 		for(auto &row : board.data_)
 			for(auto &cell : row)
 				if(cell.figure_.has_value())
 				{
 					// sum++;
 					Figure figure = *cell.figure_;
+					if(figure.type_ == FigureType::INTELLECTOR)
+					{
+						if(figure.colour_ == colour)
+							allied_intellector = 1;
+						else
+							enemy_intellector = 1;
+					}
 
 					if(figure.colour_ == colour)
 						sum += figure_value.at(figure.type_);
 					else
 						sum -= figure_value.at(figure.type_);
 				}
+		if(!enemy_intellector)
+			return 1e9;
+		else if(!allied_intellector)
+			return -1e9;
 		return sum;
 	}	
 };
--- src/AlphaBetaBot.cpp
+++ src/AlphaBetaBot.cpp
@@ -12,19 +12,43 @@ PlayerColour other_colour(PlayerColour colour)
         return PlayerColour::black_;
 }
 
+const int DEPTH = 4;
+PlayerColour Colour;
+
 std::pair<int, std::shared_ptr<SimpleMove>> AlphaBetaBot::make_virtual_move(const Game &game,
                                                                             PlayerColour colour,
                                                                             bool max,
                                                                             int alpha,
                                                                             int beta,
                                                                             int depth) {
+    int value = evaluation_function_(game.getBoard(), Colour);
+
+    if(abs(value) > 1e5)
+    {
+        return {value, nullptr};
+    }
+
     if (depth == 0) {
         return std::pair<int, std::shared_ptr<SimpleMove>>{
-            evaluation_function_(game.getBoard(), colour),
+            evaluation_function_(game.getBoard(), Colour),
             nullptr};
     }
 
     std::vector<std::shared_ptr<SimpleMove>> all_moves;
+
+    sort(all_moves.begin(), all_moves.end(), [&](auto a, auto b)
+    {
+        Game gamecopy1(game);
+        gamecopy1.makeMove(*a);
+        Game gamecopy2(game);
+        gamecopy2.makeMove(*b);
+        int eval1 = evaluation_function_(gamecopy1.getBoard(), colour);
+        int eval2 = evaluation_function_(gamecopy2.getBoard(), colour);
+        if(max)
+            return eval1 > eval2;
+        else 
+            return eval1 < eval2;
+    });
 
     Board board = Board(game.getBoard());
 
@@ -80,5 +104,6 @@ std::pair<int, std::shared_ptr<SimpleMove>> AlphaBetaBot::make_virtual_move(cons
 std::shared_ptr<SimpleMove> AlphaBetaBot::makeMove(const Game &game) {
 	Game gamecopy(game);
     auto colour = game.getColourCurrentPlayer();
-	return make_virtual_move(gamecopy, colour, true, -1000, 1000, 4).second;
+    Colour = colour;
+	return make_virtual_move(gamecopy, colour, true, -1000, 1000, DEPTH).second;
 }
