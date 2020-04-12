17e1036b6513da7e57a81e242b68082e8b15235b
Mikhail <ca8paco8@gmail.com>
2020-04-12 14:44:16 +0300

    Added EvalGame and FunctionSet

---
+++ include/EvalGame.hpp
@@ -0,0 +1,33 @@
+#ifndef _EVALGAME_HPP
+#define _EVALGAME_HPP
+
+#include "Game.hpp"
+#include "FunctionSet.hpp"
+#include "Evaluate.hpp"
+
+class EvalGame
+{
+public:
+    Game game_;
+    const PlayerColour bot_colour_;
+
+    FunctionSet functions_;
+
+    int eval_;
+
+    EvalGame(const Game game, 
+             const PlayerColour &bot_colour, 
+             int set_number) : game_(game), bot_colour_(bot_colour), functions_(set_number), eval_(functions_.evaluate(game_, bot_colour_)) {};
+
+
+    bool makeMove(const Move& move)
+    {
+        eval_ += functions_.delta(move, bot_colour_);
+        return game_.makeMove(move);
+    }
+
+    // bool undoMove()
+
+};
+
+#endif //_EVALGAME_HPP
