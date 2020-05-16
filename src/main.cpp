cf9661d277ac211923d9025cf3c6ecc29eed6aef
Худяков Юрий <catchitoo@mail.ru>
2020-05-16 22:46:34 +0500

    Add exception handling in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,17 @@
 
 #include <iostream>
 
-#include "ViewMenuMultiplexerCurses.hpp"
 #include "ViewInitCurses.hpp"
+#include "ViewMenuCurses.hpp"
+#include "ViewMenuMultiplexerCurses.hpp"
 
 int main() {
     try {
         viewCurses::MenuMultiplexerCurses menuMultiplexer;
         viewCurses::RET_CODE rc = menuMultiplexer.show();
         static_cast<void>(rc);
-//        if (initCursesDone != 0)
-//            throw MenuException("Not all menus were closed and ncurses is still running");
+        //        if (initCursesDone != 0)
+        //            throw MenuException("Not all menus were closed and ncurses is still running");
         //    Board* board = new Board;
         //        viewCurses::ViewModelCurses tmp = viewCurses::ViewModelCurses
         //        { *board, PlayerColour::white_ }; viewCurses::ViewCurses
@@ -19,6 +20,17 @@ int main() {
         //        PlayerColour::white_)); view.run();
     } catch (const ViewBaseException& e) {
         std::cout << "View error: " << e.what() << std::endl;
+    } catch (const viewCurses::MenuException& e) {
+        std::cout << "Menu error: " << e.what() << std::endl;
+    }
+    catch (const std::runtime_error& e) {
+        std::cout << "Runtime error: " << e.what() << std::endl;
+    }
+    catch (const std::exception& e) {
+        std::cout << "Unknown exception: " << e.what() << std::endl;
+    }
+    catch (...) {
+        std::cout << "Unknown error" << std::endl;
     }
     return 0;
 }
