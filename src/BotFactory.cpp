723798a932ec981a0291f67fb365523a3a131d08
Худяков Юрий <catchitoo@mail.ru>
2020-05-26 18:37:34 +0500

    After-merge fixes: BotFactory changed in both branches

--- src/BotFactory.cpp
+++ src/BotFactory.cpp
@@ -15,20 +15,20 @@ std::shared_ptr<Bot> BotFactory(int difficulty) {
         case 0:
             bot = std::reinterpret_pointer_cast<Bot>(std::make_shared<RandomBot>());
             break;
-        case 2:
+        case 1:
             bot = std::reinterpret_pointer_cast<Bot>(std::make_shared<GreedyBot>(evaluate::scoreSumFigurePoints));
             break;
-        case 3:
-            bot = std::reinterpret_pointer_cast<Bot>(std::make_shared<OptimizedAlphaBetaBot>(3, 0));
-            break;
-        case 6:
-            bot = std::reinterpret_pointer_cast<Bot>(std::make_shared<OptimizedAlphaBetaBot>(4, 0));
-            break;
-        case 10:
-            bot = std::reinterpret_pointer_cast<Bot>(std::make_shared<OptimizedAlphaBetaBot>(5, 0));
-            break;
         default:
-            assert(false);
+            bot = std::reinterpret_pointer_cast<Bot>(std::make_shared<OptimizedAlphaBetaBot>(difficulty+1, 0));
+//            break;
+//        case 6:
+//            bot = std::reinterpret_pointer_cast<Bot>(std::make_shared<OptimizedAlphaBetaBot>(4, 0));
+//            break;
+//        case 10:
+//            bot = std::reinterpret_pointer_cast<Bot>(std::make_shared<OptimizedAlphaBetaBot>(5, 0));
+//            break;
+//        default:
+//            assert(false);
     }
     return bot;
 }
